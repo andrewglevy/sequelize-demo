a304916b9e8a0f2b7cdfbe6f2409607530bc991a
andrewglevy <andrewglevy@gmail.com>
2020-09-22 23:40:06 -0700

    tutorial

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 // % node app.js
+// docs.sequelizejs.com
 
 const Sequelize = require('sequelize');
 require('dotenv').config();
@@ -9,21 +10,56 @@ const connection = new Sequelize(process.env.DB_SCHEMA, process.env.DB_USER, pro
 });
 
 const Article = connection.define('article', {
-  title: Sequelize.STRING,
-  body: Sequelize.TEXT,
+  // identifier: {
+  //   type: Sequelize.STRING,
+  //   primaryKey: true,
+  // example of custom primary key, otherwise auto incrementing id
+  // },
+  title: {
+    type: Sequelize.STRING,
+    unique: true,
+    allowNull: false,
+    validate: {
+      len: {
+        args: [4, 150],
+        message: 'Title must be between 4 and 150 characters.',
+      },
+    },
+  },
+  body: {
+    type: Sequelize.TEXT,
+    defaultValue: 'lorem ibsem.',
+    validate: {
+      // defining a custom validation rule with a function
+      startsWithUpper: (bodyVal) => {
+        const first = bodyVal.charAt(0);
+        const startsWithUpper = first === first.toUpperCase();
+        if (!startsWithUpper) {
+          throw new Error('First letter must be an uppercase letter');
+        }
+      },
+    },
+  },
+}, {
+  hooks:
+  {
+    // beforeValidate() {
+    //
+    // },
+    afterCreate(res) {
+      console.log(`Created article with title ${res.dataValues.title}`);
+    },
+    // more hooks listed in Sequelize documentation
+  },
 });
+connection.sync({
+  force: true,
+  // this will potentially drop the table if it exists; not for prod use
+  logging: console.log,
+}).then(function () {
+  return Article.create({
+    title: 'The Title',
+    body: 'Quem appellat hic imitarentur ad tamen vidisse quo cohaerescant, se hic instituendarum, fugiat ut pariatur iis summis, anim mandaremus arbitrantur id legam e voluptate ab culpa ab fabulas tempor culpa possumus quae a ipsum si ea legam commodo. Ex offendit te fabulas ut cupidatat sint est laborum praetermissum. Constias o aute constias, vidisse sunt elit deserunt eram, laboris fugiat labore ubi export qui id nulla philosophari, cupidatat adipisicing ab occaecat, labore export illum.',
 
-connection.sync().then(function (){
-  // Article.create({
-  //   title: 'demo title',
-  //   body: 'asdfASDF.'
-
-  // Article.findByPk(1)
-  //   .then(function(article) {
-  //     console.log(article.dataValues);
-  //   });
-
-  Article.findAll().then(function(articles) {
-    console.log(articles.length);
   });
-}).catch(err => console.log('An error occurred: \n' + err.message));
+}).catch((err) => console.log(err.message));
